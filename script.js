adc061897da0c22a1de9de663d920c21a0e2642e
HuyVuong <vuonghuy12345@gmail.com>
2024-01-19 15:39:05 +0700

    update

--- script.js
+++ script.js
@@ -11,180 +11,157 @@ const dau = ["*", ")", "(", "-", "+", "%", "/", "^"];
 const dau1 = ["*", "-", "+", "%", "/", "^"];
 
 inputText.addEventListener("keydown", (e) => {
-  if (e.keyCode === 13) {
+  if (e.keyCode === 13 && inputText.value.length > 0) {
     handleConvertToPrefix();
   }
 });
 
-document.getElementById('inputFile')
-.addEventListener('change', function () {
+document.getElementById("inputFile").addEventListener("change", function () {
+  let fr = new FileReader();
+  fr.onload = function () {
+    document.getElementById("inputText").value = fr.result;
+  };
 
-    let fr = new FileReader();
-    fr.onload = function () {
-        document.getElementById('inputText').value  = fr.result;
-    }
-
-    fr.readAsText(this.files[0]);
-})
+  fr.readAsText(this.files[0]);
+});
 
 const handleConvertToPrefix = () => {
-  containerTable.innerHTML = "";
-  document.querySelector(".tbodyTableResult").innerHTML = "";
-  stack = [];
-  output = [];
-  let value = input.value.trim();
-  value = value.replace(/\s/g, "");
-  const ArrayString = [];
-  let so = "";
-
-  if (!resultText.classList.contains('none-active')) {
-    resultText.classList.add('none-active');
-  }
-
-  for (let i = 0; i < value.length; i++) {
-    if (isNaN(value[i])) {
-      ArrayString.push(value[i]);
-    } else {
-      if (isNaN(value[i + 1]) && value[i + 1] !== ".") {
-        ArrayString.push(value[i]);
-      } else {
-        for (let j = i; j < value.length; j++) {
-          so += value[j];
-          if (isNaN(value[j + 1]) && value[j + 1] !== ".") {
-            i = j;
-            break;
-          }
-        }
-        ArrayString.push(so);
-        so = "";
-      }
-    }
-  }
-  if (
-    dau.includes(ArrayString[0]) ||
-    dau.includes(ArrayString[ArrayString.length - 1])
-  ) {
-    if (ArrayString[0] !== "(" || ArrayString[ArrayString.length - 1] !== ")") {
-      alert("Sai biểu thức");
-
-      if (resultText.classList.contains('none-active')) {
-        resultText.classList.remove('none-active');
-      }
-      containerTable.innerHTML = "";
-      document.querySelector(".tbodyTableResult").innerHTML = "";
-      document.querySelector(".tableResult").style.display = "none";
-      result.innerHTML = "";
-      return;
-    }
-  }
-
-  if (
-    ArrayString.filter((e) => e === "(").length !==
-    ArrayString.filter((e) => e === ")").length
-  ) {
-    alert("Sai biểu thức");
-
-    if (resultText.classList.contains('none-active')) {
-      resultText.classList.remove('none-active');
-    }
-
+  if (inputText.value.length > 0) {
     containerTable.innerHTML = "";
     document.querySelector(".tbodyTableResult").innerHTML = "";
-    result.innerHTML = "";
-    document.querySelector(".tableResult").style.display = "none";
-    return;
-  }
-  for (let i = 0; i < ArrayString.length; i++) {
-    if (dau.includes(ArrayString[i]) && dau.includes(ArrayString[i + 1])) {
-      if (ArrayString[i] === "(" && ArrayString[i + 1] !== "(") {
-        alert("Sai biểu thức");
+    stack = [];
+    output = [];
+    let value = input.value.trim();
+    value = value.replace(/\s/g, "");
+    const ArrayString = [];
+    let so = "";
 
-        if (resultText.classList.contains('none-active')) {
-          resultText.classList.remove('none-active');
-        }
+    if (!resultText.classList.contains("none-active")) {
+      resultText.classList.add("none-active");
+    }
 
-        containerTable.innerHTML = "";
-        document.querySelector(".tbodyTableResult").innerHTML = "";
-        result.innerHTML = "";
-        document.querySelector(".tableResult").style.display = "none";
-        return;
-      } else if (ArrayString[i] === ")" && ArrayString[i + 1] === "(") {
-        alert("Sai biểu thức");
-        
-        if (resultText.classList.contains('none-active')) {
-          resultText.classList.remove('none-active');
+    for (let i = 0; i < value.length; i++) {
+      if (isNaN(value[i])) {
+        ArrayString.push(value[i]);
+      } else {
+        if (isNaN(value[i + 1]) && value[i + 1] !== ".") {
+          ArrayString.push(value[i]);
+        } else {
+          for (let j = i; j < value.length; j++) {
+            so += value[j];
+            if (isNaN(value[j + 1]) && value[j + 1] !== ".") {
+              i = j;
+              break;
+            }
+          }
+          ArrayString.push(so);
+          so = "";
         }
-        
-        containerTable.innerHTML = "";
-        document.querySelector(".tbodyTableResult").innerHTML = "";
-        result.innerHTML = "";
-        document.querySelector(".tableResult").style.display = "none";
-        return;
-      } else if (
-        dau1.includes(ArrayString[i]) &&
-        dau1.includes(ArrayString[i + 1])
+      }
+    }
+    if (
+      dau.includes(ArrayString[0]) ||
+      dau.includes(ArrayString[ArrayString.length - 1])
+    ) {
+      if (
+        (ArrayString[0] !== "(" && isNaN(ArrayString[0])) ||
+        dau1.includes(ArrayString[ArrayString.length - 1])
       ) {
-        alert("Sai biểu thức");
-        
-        if (resultText.classList.contains('none-active')) {
-          resultText.classList.remove('none-active');
+        alert("Sai biểu thứcafgfg");
+
+        if (resultText.classList.contains("none-active")) {
+          resultText.classList.remove("none-active");
         }
-        
         containerTable.innerHTML = "";
         document.querySelector(".tbodyTableResult").innerHTML = "";
-        result.innerHTML = "";
         document.querySelector(".tableResult").style.display = "none";
+        result.innerHTML = "";
         return;
       }
     }
-  }
 
-  ArrayString.forEach((token) => {
-    createMessage(`Xét "${token}": `);
-    if (!dau.includes(token)) {
-      output.push(token);
-      createMessage(`"${token}" là số nên đẩy ra Output`);
-      createTable();
-      createTr(token, stack.join(", "), output.join(", "));
-      createTrForTableResult(token, stack.join(", "), output.join(", "));
-    } else {
-      if (stack.length === 0) {
-        stack.push(token);
-        createMessage(
-          `"${token}" không là số và stack rỗng nên đẩy "${token}" vào stack `
-        );
+    if (
+      ArrayString.filter((e) => e === "(").length !==
+      ArrayString.filter((e) => e === ")").length
+    ) {
+      alert("Sai biểu thức");
+
+      if (resultText.classList.contains("none-active")) {
+        resultText.classList.remove("none-active");
+      }
+
+      containerTable.innerHTML = "";
+      document.querySelector(".tbodyTableResult").innerHTML = "";
+      result.innerHTML = "";
+      document.querySelector(".tableResult").style.display = "none";
+      return;
+    }
+    for (let i = 0; i < ArrayString.length; i++) {
+      if (dau.includes(ArrayString[i]) && dau.includes(ArrayString[i + 1])) {
+        if (ArrayString[i] === "(" && ArrayString[i + 1] !== "(") {
+          alert("Sai biểu thức1");
+
+          if (resultText.classList.contains("none-active")) {
+            resultText.classList.remove("none-active");
+          }
+
+          containerTable.innerHTML = "";
+          document.querySelector(".tbodyTableResult").innerHTML = "";
+          result.innerHTML = "";
+          document.querySelector(".tableResult").style.display = "none";
+          return;
+        } else if (ArrayString[i] === ")" && ArrayString[i + 1] === "(") {
+          alert("Sai biểu thức2");
+
+          if (resultText.classList.contains("none-active")) {
+            resultText.classList.remove("none-active");
+          }
+
+          containerTable.innerHTML = "";
+          document.querySelector(".tbodyTableResult").innerHTML = "";
+          result.innerHTML = "";
+          document.querySelector(".tableResult").style.display = "none";
+          return;
+        } else if (
+          dau1.includes(ArrayString[i]) &&
+          dau1.includes(ArrayString[i + 1])
+        ) {
+          alert("Sai biểu thức3");
+
+          if (resultText.classList.contains("none-active")) {
+            resultText.classList.remove("none-active");
+          }
+
+          containerTable.innerHTML = "";
+          document.querySelector(".tbodyTableResult").innerHTML = "";
+          result.innerHTML = "";
+          document.querySelector(".tableResult").style.display = "none";
+          return;
+        }
+      }
+    }
+
+    ArrayString.forEach((token) => {
+      createMessage(`Xét "${token}": `);
+      if (!dau.includes(token)) {
+        output.push(token);
+        createMessage(`"${token}" là số nên đẩy ra Output`);
         createTable();
         createTr(token, stack.join(", "), output.join(", "));
         createTrForTableResult(token, stack.join(", "), output.join(", "));
       } else {
-        if (token === "(") {
+        if (stack.length === 0) {
           stack.push(token);
-          createMessage(`"${token}" là dấu "(" nên đẩy vào stack `);
-          createTable();
-          createTr(token, stack.join(", "), output.join(", "));
-          createTrForTableResult(token, stack.join(", "), output.join(", "));
-        } else if (stack[stack.length - 1] === "(") {
           createMessage(
-            `Do toán tử ngoài cùng của stack là "(" nên đẩy "${token}" vào stack`
+            `"${token}" không là số và stack rỗng nên đẩy "${token}" vào stack `
           );
-          stack.push(token);
-          createTable();
-          createTr(token, stack.join(", "), output.join(", "));
-          createTrForTableResult(token, stack.join(", "), output.join(", "));
-        } else if (compare(token) > compare(stack[stack.length - 1])) {
-          createMessage(
-            `"${token}" có độ ưu tiên >" ${
-              stack[stack.length - 1]
-            }" nên đẩy "${token}" vào stack`
-          );
-          stack.push(token);
           createTable();
           createTr(token, stack.join(", "), output.join(", "));
           createTrForTableResult(token, stack.join(", "), output.join(", "));
         } else {
           if (token === ")") {
             createMessage(
-              `Token là dấu ) nên chuyển lần lượt các dấu từ đỉnh stack sang output đến khi gặp dấu "(" thì xóa dấu "(" đó đi và dừng lại`
+              `Token là dấu ")" nên chuyển lần lượt các dấu từ đỉnh stack sang output đến khi gặp dấu "(" thì xóa dấu "(" đó đi và dừng lại`
             );
             createTable();
             for (let i = stack.length - 1; i >= 0; i--) {
@@ -205,12 +182,36 @@ const handleConvertToPrefix = () => {
                 isFirst = false;
               }
             }
+          } else if (token === "(") {
+            stack.push(token);
+            createMessage(`"${token}" là dấu "(" nên đẩy vào stack `);
+            createTable();
+            createTr(token, stack.join(", "), output.join(", "));
+            createTrForTableResult(token, stack.join(", "), output.join(", "));
+          } else if (stack[stack.length - 1] === "(") {
+            createMessage(
+              `Do toán tử ngoài cùng của stack là "(" nên đẩy "${token}" vào stack`
+            );
+            stack.push(token);
+            createTable();
+            createTr(token, stack.join(", "), output.join(", "));
+            createTrForTableResult(token, stack.join(", "), output.join(", "));
+          } else if (compare(token) > compare(stack[stack.length - 1])) {
+            createMessage(
+              `"${token}" có độ ưu tiên >" ${
+                stack[stack.length - 1]
+              }" nên đẩy "${token}" vào stack`
+            );
+            stack.push(token);
+            createTable();
+            createTr(token, stack.join(", "), output.join(", "));
+            createTrForTableResult(token, stack.join(", "), output.join(", "));
           } else {
             output.push(stack[stack.length - 1]);
             createMessage(
-              `"${token}" có độ ưu tiên < hoặc = "${
+              `"${token}" có độ ưu tiên < hoặc = toán tử ngoài cùng của stack ("${
                 stack[stack.length - 1]
-              }" nên chuyển "${
+              }") nên chuyển "${
                 stack[stack.length - 1]
               }" vào stack và kiểm tra lại: Nếu stack rỗng hoặc toán tử ngoài cùng stack là  "(" hoặc "${token}" có độ ưu tiên > toán tử ngoài cùng stack thì chuyển "${token}" vào stack, nếu "${token}" có độ ưu tiên < toán tử ngoài cùng stack thì thêm toán tử ngoài cùng stack vào output và thực hiện lại`
             );
@@ -222,23 +223,24 @@ const handleConvertToPrefix = () => {
           }
         }
       }
-    }
-  });
-  const stackReverse = [...stack].reverse();
-  output = [...output, ...stackReverse];
-  if (stackReverse.length > 0) {
-    createMessage(
-      `Token rỗng chuyển lần lượt các phần tử còn lại từ đầu stack ra output`
-    );
-    createTable();
-    createTr("", stack.join(", "), output.join(", "));
-    createTrForTableResult("", stack.join(", "), output.join(", "));
-  }
+    });
+    const stackReverse = [...stack].reverse();
 
-  result.innerHTML = `Vậy biểu thức hậu tố của ${value} là: ${output.join(
-    " "
-  )}`;
-  document.querySelector(".tableResult").style.display = "block";
+    output = [...output, ...stackReverse];
+    if (stackReverse.length > 0) {
+      createMessage(
+        `Token rỗng chuyển lần lượt các phần tử còn lại từ đầu stack ra output`
+      );
+      createTable();
+      createTr("", stack.join(", "), output.join(", "));
+      createTrForTableResult("", stack.join(", "), output.join(", "));
+    }
+
+    result.innerHTML = `Vậy biểu thức hậu tố của ${value} là: ${output.join(
+      " "
+    )}`;
+    document.querySelector(".tableResult").style.display = "block";
+  }
 };
 const compare = (text) => {
   if (text == "^") return 3;
@@ -257,7 +259,7 @@ const check = (token) => {
     token !== "(" &&
     token !== ")"
   ) {
-    output += token;
+    output.push(token);
   } else {
     if (stack.length === 0) {
       stack.push(token);
@@ -273,12 +275,12 @@ const check = (token) => {
               stack.pop();
               return;
             } else {
-              output += stack[stack.length - 1];
+              output.push(stack[stack.length - 1]);
               stack.pop();
             }
           }
         } else {
-          output += stack[stack.length - 1];
+          output.push(stack[stack.length - 1]);
           stack.pop();
           check(token);
         }
